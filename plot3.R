a609eac513c656772d31abc157356df6604359e7
phil <pmgoddard89@gmail.com>
2015-02-04 20:18:47 +0000

    fixed legend on plot 3

--- plot3.R
+++ plot3.R
@@ -20,6 +20,6 @@ lines(subDat$Date.Time,subDat$Sub_metering_3, type="l",col="blue")
 
 legend("topright",
        legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
-       lty=c(1,1),col=c("black","blue","red"))
+       lty=c(1,1),col=c("black","red","blue"))
 
 dev.off()
